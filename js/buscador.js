388fb030c3f4349b19818ac5b8a1b2f0a8e11221
Marco Leskovar <leskovarmarco@gmail.com>
2023-04-28 15:54:17 -0300

    buscador con problema de 767px

--- js/buscador.js
+++ js/buscador.js
@@ -8,27 +8,23 @@ const lupaBtn = document.querySelector('#lupaButton');
 listOpenBtn.addEventListener('click', () => {
 listCloseBtn.style.display = 'flex';
 listOpenBtn.style.display = 'none';
-lupaOpenBtn.style.display = 'none';
-lupaBtn.style.opacity = '1';
+lupaBtn.style.display = 'none';
 });
 
 listCloseBtn.addEventListener('click', () => {
 listCloseBtn.style.display = 'none';
 listOpenBtn.style.display = 'flex';
-lupaOpenBtn.style.display = 'flex';
-lupaBtn.style.opacity = '1';
+lupaBtn.style.display = 'flex';
 });
 
 lupaOpenBtn.addEventListener('click', () => {
 lupaCloseBtn.style.display = 'flex';
 lupaOpenBtn.style.display = 'none';
-listOpenBtn.style.display = 'none';
-listBtn.style.opacity = '0.5';
+listBtn.style.display = 'none';
 });
 
 lupaCloseBtn.addEventListener('click', () => {
 lupaCloseBtn.style.display = 'none';
 lupaOpenBtn.style.display = 'flex';
-listOpenBtn.style.display = 'flex';
-listBtn.style.opacity = '1';
+listBtn.style.display = 'flex';
 });
